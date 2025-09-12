d945b6809a81b86ab384d72277c661bab19b9a07
srivamsio4 <pentakotasri204@gmail.com>
2025-09-12 13:15:45 +0530

    first one of basics

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onMouseEnter={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button id="hello" onClick={() => alert("Hello Vamsi!")}>
+        hello vamsi
+      </button>
     </>
   )
 }
