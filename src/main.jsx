b657cc35502cfee42b0b253ca84d72c67049b9d2
srivamsio4 <pentakotasri204@gmail.com>
2025-09-16 12:23:42 +0530

    Create a variable that contains HTML code and display it in the root node:

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+const element=(
+  <table>
+    <tr>
+      <th>name</th>
+    </tr>
+    <tr>
+      <td>
+        vamsi
+      </td>
+    </tr>
+    <tr>
+      <td>
+        sir
+      </td>
+    </tr>
+  </table>
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  element 
 )
